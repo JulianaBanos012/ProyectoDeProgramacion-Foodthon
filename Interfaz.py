0a67db6495497f85a40be56b4f52400d42e0befd
JulianaBanos012 <52583630+JulianaBanos012@users.noreply.github.com>
2020-04-11 17:35:21 -0500

    Update Interfaz.py

--- Interfaz.py
+++ Interfaz.py
@@ -1,1 +1,25 @@
 from tkinter import *
+
+ventana = Tk()
+ventana.geometry("400x200")
+ventana.configure(bg="lightyellow")
+ventana.title("¿Qué tipo de comida desea?")
+
+List1 = ['Costeño', 'Cachaco', 'Paisa', 'Continental', 'Americano']
+
+def Desayunos():
+    print(List1)
+
+List2 = ['Vegetariano', 'Carnívoro', 'Gringo', 'Continental', 'Asiático']
+
+def Almuerzos():
+    print(List2)
+
+BotonDesayunos = Button(ventana, text="Desayunos", command=Desayunos)
+BotonDesayunos.pack(side=LEFT)
+
+BotonAlmuerzos = Button(ventana, text="Almuerzos", command=Almuerzos)
+BotonAlmuerzos.pack(side=LEFT)
+
+
+ventana.mainloop()
